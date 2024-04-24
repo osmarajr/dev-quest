59fe3d6e2b90af2a24f96a76dc17a2762f744e86
Osmar Almeida <osmar.ajr10@gmail.com>
2024-04-23 22:19:34 -0300

    add aula 02 e 03 de JS Básico

---
+++ 09-modulo-javascript-basico/02-console-comentario/index.js
@@ -0,0 +1,5 @@
+console.log("Ola 'Mundo!'")
+//Imprimir mensagem no console do navegador
+/*
+    Comentário de bloco
+*/
---
+++ 09-modulo-javascript-basico/03-variaveis/index.js
@@ -0,0 +1,26 @@
+// var function - Não pode renomear variaveis com alguns nomes especificos pois são utilizados pelo javascript
+
+// var nome do cliente  - Não pode usar nomes de variaveis com espaço
+
+// var nomeDoCliente - o certo a se declarar
+var nomePessoa = "Roberto";
+
+// const nomeVariavel - Variavel constante
+const emailPessoa = "cadu@gmail.com";
+
+let telefonePessoa = "51987222222";
+
+telefonePessoa = "5199999999";
+
+console.log(telefonePessoa)
+
+{
+    var nomePessoa = "Roberto";
+    const emailPessoa = "cadu@gmail.com";
+    let telefonePessoa = "51987222222"; 
+}
+// Não pode imprimir variaveis dentro de um bloco
+console.log(telefonePessoa)
+
+escolha = confirm("Deseja cancelar?")
+console.log(escolha)
