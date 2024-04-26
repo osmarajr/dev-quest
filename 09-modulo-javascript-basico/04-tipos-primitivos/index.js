58d001d3992c811a22f8ca06e254fc3ef19899bc
Osmar Almeida <osmar.ajr10@gmail.com>
2024-04-25 22:21:55 -0300

    adicionado aula 04

---
+++ 09-modulo-javascript-basico/04-tipos-primitivos/index.js
@@ -0,0 +1,20 @@
+let nome = 'Roberto' // tipo primitivo string
+let numero = 0.6666666 // tipo primitivo number
+let usuarioVerificado = false // tipo primitivo boolean
+let pessoa = null
+let telefone = undefined
+
+if(usuarioVerificado){
+    console.log("usuário verificado")
+}else{
+    console.log("usuário não é verificado")
+}
+
+/*
+    String
+    Number
+    Boolean
+    Null
+    Undefined
+
+*/
