04e36bb774117fe15e69b1086cdc3aa3e4fccc78
Osmar Almeida <osmar.ajr10@gmail.com>
2024-05-01 17:27:22 -0300

    add aula 06, 07,08

---
+++ 09-modulo-javascript-basico/08-lacos-while/index.js
@@ -0,0 +1,22 @@
+//while(condicao){
+//  <bloco de execucao>
+//}
+
+// let a = 10
+// let b = 15
+
+// while(a<b){
+//     console.log('incrementando a variável a ' + a)
+//     a++
+// }
+
+// do{
+//      <bloco de execucao>    
+//}while(condicao)
+
+var i = 1
+
+do{
+    console.log('entrou ' + i)
+    i++
+}while(i <= 5)
