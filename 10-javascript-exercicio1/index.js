62bc0a52483d5212fdfb28c60a63712527f407b3
Osmar Almeida <osmar.ajr10@gmail.com>
2024-05-07 21:49:56 -0300

    iniciando exercicio 01 javascript basico

---
+++ 10-javascript-exercicio1/index.js
@@ -0,0 +1,10 @@
+// Peça que o usuário informe o seu nome usando o prompt
+
+// Apresente a mensagem de boas vindas seguindo do
+// nome que a pessoa digitou no passo 1.
+
+// Obs: pra apresentar a mensagem de boas vindas pode usar
+// o alert também.
+
+let nomeUsuario = prompt('Informe seu nome: ');
+
