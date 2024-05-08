dd7849b273b49f8fd1ee17fc2644bd4190fee087
Osmar Almeida <osmar.ajr10@gmail.com>
2024-05-08 15:51:40 -0300

    add exercicio 04 de JS basico

---
+++ 10-javascript-exercicio1/index-ex04.js
@@ -0,0 +1,14 @@
+/*
+Laços de repetição
+
+1 - Crie um for que conte de 0 até 10 e apresente a a
+seguinte mensagem para cada número dentro do laço.
+"O número atual é o 0"
+"O número atual é o 1"
+... e assim por diante até o número 10
+
+Para apresentar esses textos use o console.log
+*/
+for(i=0;i<=10;i++){
+    console.log('O número atual é o '+ i)
+}
