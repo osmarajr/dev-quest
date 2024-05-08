96666233adb58b55f40533923af3cb1c97015816
Osmar Almeida <osmar.ajr10@gmail.com>
2024-05-08 16:01:07 -0300

    add exercicio 05 de JS basico

---
+++ 10-javascript-exercicio1/index-ex05.js
@@ -0,0 +1,27 @@
+/*
+Laços de repetição e operadores matemáticos
+
+Leia todos os passos com atenção. Não esqueça
+de quebrar o problema em pequenas partes para
+facilitar a implementação.
+
+1 - Crie um for que conte de 0 até 10
+
+2 - Dentro desse laço você deve dobrar o número
+atual.
+
+3 - Apresente a seguinte mensagem para cada número
+dentro do laço:
+
+"O dobro do número x é y"
+
+Dica: O x é o numero atual dentro do laço e o y é o
+dobro do x.
+
+Para apresentar esses textos use o console.log
+*/
+
+for(x=0;x<=10;x++){
+    y=x*2
+    console.log('O dobro do número '+ x +' é ' + y)
+}
