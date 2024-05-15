fd496d9bba89648acac034e8a719a5819637543a
Osmar Almeida <osmar.ajr10@gmail.com>
2024-05-14 21:38:17 -0300

    add aula 04 js intermediario

---
+++ 11-modulo-javascript-intermediario/04-arrow-functions/index.js
@@ -0,0 +1,11 @@
+let resultadoDaSoma = (numero1, numero2) => {
+    return numero1 + numero2
+}
+
+console.log(resultadoDaSoma(12,5))
+
+const incentivarQuest = nomeQuester => {
+    console.log('Parabéns  '+ nomeQuester +', Você chegou ao modulo de JS intermediario')
+}
+
+incentivarQuest('Roberto')
