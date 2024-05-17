ecf49c98d6be81a8bf1eb8c1793d01f2f924a3ed
Osmar Almeida <osmar.ajr10@gmail.com>
2024-05-16 21:59:34 -0300

    add aula 05 e 06 de JS intermediario

---
+++ 11-modulo-javascript-intermediario/06-array/index.js
@@ -0,0 +1,10 @@
+let filmes = ['Vingadores', 'Mulher Maravilha']
+let avaliacoes = [1, 4.6, 511]
+
+console.log(filmes[0])
+
+let descricaofilme = ['Vingadores', 4.5, true]
+
+console.log(descricaofilme)
+
+console.log(filmes[2])//undefined, pois não possui valor nessa posição
