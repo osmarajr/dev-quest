1b8ae71ad5f612e0ea44922408efb3071d89bdb8
Osmar Almeida <osmar.ajr10@gmail.com>
2024-05-20 22:24:46 -0300

    add aula 09 js intermediario

---
+++ 11-modulo-javascript-intermediario/09-splice-slice/index.js
@@ -0,0 +1,11 @@
+let frutas = ['Banana', 'Maçã', 'Laranja', 'Uva']
+
+// let frutasExtraidas = frutas.slice(1,3)
+
+// console.log(frutasExtraidas)
+
+// console.log(frutas)
+
+// frutas.splice(indice, numerodetermos)
+let frutasExtraidas = frutas.splice(1, 2, 'goiaba','Abacaxi', 'Perâ')
+console.log(frutasExtraidas)
