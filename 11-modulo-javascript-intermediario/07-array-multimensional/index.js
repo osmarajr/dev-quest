e0edbbfbe3299307b7ab238c78036011295c6d72
Osmar Almeida <osmar.ajr10@gmail.com>
2024-05-20 21:59:47 -0300

    add aula 07 js intermediario

---
+++ 11-modulo-javascript-intermediario/07-array-multimensional/index.js
@@ -0,0 +1,12 @@
+/*let filmes = ['Vingadores', 'Mulher Maravilha']
+let descricaoFilme = ['Vingadores', 4.5, true]
+*/
+let filmes = [
+    ['Vingadores', 4.5, true], 
+    ['Mulher', 4.6 , false, ['Gal Gadot',25]]
+]
+
+console.table(filmes)
+console.table(filmes[0][2])
+console.table(filmes[1][0])
+console.table(filmes[1][3][1])
