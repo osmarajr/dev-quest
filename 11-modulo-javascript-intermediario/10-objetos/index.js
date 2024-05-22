07d240415a40010d06c59250a14b9d49fb06fbe6
Osmar Almeida <osmar.ajr10@gmail.com>
2024-05-21 22:14:13 -0300

    add aula 10 e 11 de Js intermediario

---
+++ 11-modulo-javascript-intermediario/10-objetos/index.js
@@ -0,0 +1,15 @@
+//objeto
+let tenis = {
+    //chave : valor
+    cor: 'branco',
+    tamanho: 42,
+    emEstoque: true
+}
+//Mais recomendado utilizar
+console.log(tenis.cor)
+console.log(tenis['cor'])
+
+//adicionando no objeto
+tenis.valor = 200
+
+console.log(tenis)
