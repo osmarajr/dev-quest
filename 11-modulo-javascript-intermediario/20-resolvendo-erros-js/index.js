808c96d4a362d38c377b79cf4f1cfe588b4cc4a6
Osmar Almeida <osmar.ajr10@gmail.com>
2024-06-16 20:23:57 -0300

    add aula 20

---
+++ 11-modulo-javascript-intermediario/20-resolvendo-erros-js/index.js
@@ -0,0 +1,16 @@
+// let valorBoleto = 100;
+
+// console.log(valorBoleto);
+// console.log(valorBol);
+//ele nao avança ate concertar o erro
+// console.log(valorBoleto);
+
+let inputTelefone = document.getElementById('telefone');
+
+// console.log(inputTelefone);
+
+inputTelefone.disabled= true;
+
+function habilitarTelefone(){
+    inputTelefone.disabled = false;
+}
